e8995ec9aac2df5dfaad323874aeea054cc44164
baci29 <160430325+baci29@users.noreply.github.com>
2025-03-30 17:21:11 +0200

    Add files via upload

---
+++ CalculatorApp/CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by Radovan Bačík on 30.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalculatorApp/CalculatorAppTests/CalculatorAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppTests.swift
+//  CalculatorAppTests
+//
+//  Created by Radovan Bačík on 30.03.2025.
+//
+
+import Testing
+@testable import CalculatorApp
+
+struct CalculatorAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
